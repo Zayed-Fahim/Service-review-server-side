6d953b344178e23a35e6c0d06b2561d4c6a69917
Zayed Fahim <zayedfahim33.cse@gmail>
2022-11-10 21:44:58 +0600

    create file for vercel deply

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, FindCursor, ObjectId } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const services = require("./service.json");
-app.get("/services", (req, res) => {
-    res.send(services);
-});
-
-//username:deliveryDotComAdmin
-//password:BX8FlmtE4rVpKE2K
-
-const uri =
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.llom3t1.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DELIVERY_DOT_COM_ADMIN}:${process.env.DELIVERY_DOT_COM_ADMIN_PASSWORD}@cluster0.llom3t1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
-});
-client.connect((err) => {
-    const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-    client.close();
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    const servicesCollection = client.db('deliveryDotCom').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+
+        app.get('/services/filtered', async (req, res) => {
+            const query = {}
+            const cursor = servicesCollection.find(query).limit(3).sort({_id: -1});
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+      
+      app.get("/services/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await servicesCollection.findOne(query);
+        res.send(service)
+      });
+    app.post("/services/:id/reviews", async (req, res) => {
+      const servicesCollection = client.db('deliveryDotCom').collection('reviews');
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)}
+      const user = req.body;
+      console.log(user);
+      const result = await servicesCollection.insertMany(query,user);
+      res.send(result);
+    });
+    }
+    finally {
+        
+    }
+}
+run()
+.catch(error => console.error(error))
 
 
 
